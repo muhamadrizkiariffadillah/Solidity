f55becf0c4ab1f38ea40eec134ceb4facff51594
muhamadrizkiariffadillah <kilisarisol@gmail.com>
2024-10-27 10:17:49 +0700

    Learn library, transfer, send, and call

--- 03 fundme/FundMe.sol
+++ 03 fundme/FundMe.sol
@@ -1,32 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "03 fundme/PriceConverter.sol";
 
 contract FundMe{
+        using PriceConverter for uint256;
         uint256 public minimumUSD = 5e18;
+        address[] public funders;
+        mapping(address => uint256) public addressToAmountSenders;
 
     function fund() public payable {
-        require(getConversionRate(msg.value) >= minimumUSD, "didn't enough send ETH");
+        require(msg.value.getConversionRate() >= minimumUSD, "didn't enough send ETH");
+        funders.push(msg.sender);
+        addressToAmountSenders[msg.sender] += msg.value;
     }
 
-    // open docs.chainlink for documentation
-    function getPrice()public view returns(uint256) {
-        // address 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        // ABS
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (,int256 price,,,) = priceFeed.latestRoundData();
-        return uint( price * 1e10);
+    function withdraw() public {
+
+        for(uint funderIndex = 0; funderIndex < funders.length;funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountSenders[funder] = 1;
+        }
+        // reset an array
+        funders = new address[](0);
+
+        // payable(msg.sender).transfer(address(this).balance);
+        // bool isSuccess = payable(msg.sender).send(address(this).balance);
+        // require(isSuccess,"send failed");
+        (bool isSuccessPay, )=payable(msg.sender).call{value: address(this).balance}("");
+        require(isSuccessPay,"call failed");
     }
 
-    function getConversionRate(uint256 ETHAmount)public view returns (uint256){
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountPrice = (ethPrice * ETHAmount) / 1e18;
-        return ethAmountPrice;
-    }
-
-    function getVersion()public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        return priceFeed.version();
-    }
 }
